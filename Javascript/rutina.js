2a63eb5e8da7ae08c3b9ada5968d562774e84cf1
AriCocaro <lic.aricocaro@gmail.com>
2025-07-15 13:36:44 -0300

    crearejercicio.js

--- Javascript/rutina.js
+++ Javascript/rutina.js
@@ -1,13 +1,39 @@
-let rutinaEjercicios = document.getElementById("EjerciciosRealizados")
-
-let cartStorage = localStorage.getItem(rutinaEjercicios)
-cartStorage = JSON.parse(cartStorage)
-
-function renderEjerciciosR(IEjercicios){
-    IEjercicios.array.forEach(ejercicio => {
-        const card = document.createElement("div")
-        cartContainer.innerHTML = <h3>${ejercicio.nombre}</h3>
-                                 <p>${peso}Kg</p>
-     ejerciciosDeRutina.appendChild(card)
-    });
+class Ejercicio {
+  constructor(id, nombre, grupoMuscular) {
+    this.id = id;
+    this.nombre = nombre;
+    this.grupoMuscular = grupoMuscular;
+  }
 }
+  validar() {
+    return this.nombre.trim() !== "" && this.grupoMuscular.trim() !== "";
+  }
+
+//IdUnico
+function generarIdUnico() {
+  const timestamp = Date.now().toString(36); 
+  const random = Math.random().toString(36).substring(2, 6);
+  return `ej_${timestamp}_${random}`;
+}
+const id = generarIdUnico();
+const nuevoEjercicio = new Ejercicio(generarIdUnico(), nombre, grupo);
+
+if (nuevoEjercicio.validar()) {
+  guardarEjercicioEnLocalStorage(nuevoEjercicio);
+} else {
+  mostrarMensaje("Por favor, completá todos los campos");
+}
+
+//Localstorage
+const LS_EJERCICIOS = "ejerciciosGuardados";
+function guardarEjercicioEnLocalStorage(ejercicio) {
+  let ejercicios = JSON.parse(localStorage.getItem(LS_EJERCICIOS)) || [];
+  ejercicios.push(ejercicio);
+  localStorage.setItem(LS_EJERCICIOS, JSON.stringify(ejercicios));
+}
+function obtenerEjerciciosDesdeLocalStorage() {
+  return JSON.parse(localStorage.getItem(LS_EJERCICIOS)) || [];
+}
+
+
+
